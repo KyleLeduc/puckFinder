231c09466e9f0090a72ed643f522f152cb7c13a5
Kyle Leduc <LeducKyleJ@gmail.com>
2021-01-08 15:05:42 -0700

    configure passport to use: session, passport-local, passport-local-mongoose methods

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 
 const rinks = require('./routes/rinks');
@@ -49,6 +52,13 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
