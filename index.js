c56cea7989fffce644a3aa8d1e16a7bdd58c9d7f
Kyle Leduc <LeducKyleJ@gmail.com>
2021-01-09 04:06:21 -0700

    create users.js routes file
    create registration form & form get route
    
    create register route with error handling flash message

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-
-const rinks = require('./routes/rinks');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const rinkRoutes = require('./routes/rinks');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/puckFinder', { 
     useNewUrlParser: true,
@@ -67,8 +67,9 @@ app.use((req, res, next) => {
 });
 
 
-app.use('/rinks', rinks);
-app.use('/rinks/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/rinks', rinkRoutes);
+app.use('/rinks/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
     res.render('home');
