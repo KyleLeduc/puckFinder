14de9f994fb442f8eff5037251465950ce0d241a
Kyle Leduc <LeducKyleJ@gmail.com>
2021-01-19 13:57:29 -0700

    Add express-mongo-sanitize package

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const mongoSanitize = require('express-mongo-sanitize');
 const morgan = require('morgan');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
@@ -42,6 +43,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 app.use(morgan('common'));
 
 const sessionConfig = {
