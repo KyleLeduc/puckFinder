c716e10a5be3be1dacdb1fc6371d87490b20905d
Kyle Leduc <LeducKyleJ@gmail.com>
2021-01-29 16:45:32 -0700

    Change index images to thumbnail
    create new larger virtual thumbnail
    adjust columns to fit new image size
    adjust rink.description bottom margin

--- models/rink.js
+++ models/rink.js
@@ -12,6 +12,10 @@ ImageSchema.virtual('thumbnail').get(function() {
     return this.url.replace('/upload', '/upload/w_200');
 });
 
+ImageSchema.virtual('indexThumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_500');
+});
+
 const opts = { toJSON: { virtuals: true } }
 
 const RinkSchema = new Schema({
