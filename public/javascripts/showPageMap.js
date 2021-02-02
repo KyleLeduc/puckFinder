8e4071fbcb09ab45d199d483fd78ee5309c0c475
Kyle Leduc <LeducKyleJ@gmail.com>
2021-02-02 13:36:09 -0700

    Add directions option to show page map
    add mapbox-gl-directions plugin css and js files to boilerplate
    add directions button to show page
    add unsafe-eval to helmet scriptSrc contentSecurityPolicy
    configure new MapboxDirections object
    create function to adjust rink coordinates for center location
    implement navigator.geolocation.getCurrentPosition()
    use coordinates in directions.setOrigin() method
    also call setDestination method using rinkCoords

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,13 +1,50 @@
 mapboxgl.accessToken = mapToken;
+rinkCoords = rink.geometry.coordinates;
+
+
+function getDirections() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition)
+  }
+}
+
+function showPosition(position) {
+  let coords = [];
+  coords.push(position.coords.longitude);
+  coords.push(position.coords.latitude);
+  directions.setOrigin(coords);
+  directions.setDestination(rinkCoords)
+}
+
+// Shift the map south slightly to move marker out from behind directions
+function adjustedCoords(coords) {
+    let newCoords = [];
+    newCoords.push(coords[0])
+    newCoords.push(coords[1] + 0.001)
+    return newCoords;
+}
+const adjustedRinkCoords = adjustedCoords(rinkCoords)
+
+
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: rink.geometry.coordinates,
+    center: adjustedRinkCoords,
     zoom: 13.75
 });
 
-map.addControl(new mapboxgl.NavigationControl());
+let directions = new MapboxDirections({
+    accessToken: mapToken,
+    unit: 'metric',
+    profile: 'mapbox/driving',
+    controls: {
+        profileSwitcher: false,
+        instructions: false
+    }
+});
 
+map.addControl(new mapboxgl.NavigationControl());
+map.addControl(directions, 'top-left');
 new mapboxgl.Marker()
     .setLngLat(rink.geometry.coordinates)
     .setPopup(
