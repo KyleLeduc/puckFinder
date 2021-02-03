fc955f9701d3a17b811768f67bc05e8ad39c6355
Kyle Leduc <LeducKyleJ@gmail.com>
2021-02-03 11:43:07 -0700

    Change directions button to google maps link
    add rink coordinates to google maps directions URL on show.ejs
    reverse the coordinates to geojson format for map use in showPageMap.js

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,52 +1,53 @@
 mapboxgl.accessToken = mapToken;
-rinkCoords = rink.geometry.coordinates;
+rinkCoords = rink.geometry.coordinates.reverse();
 
+// // Get users geolocation (Needs error handling)
+// function getDirections() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition)
+//   }
+// }
+// function showPosition(position) {
+//   let coords = [];
+//   coords.push(position.coords.longitude);
+//   coords.push(position.coords.latitude);
+// //   Push users location and rink destination to mapbox map
+//   directions.setOrigin(coords);
+//   directions.setDestination(rinkCoords)
+// }
+// // Shift the map south slightly to move marker out from behind directions
+// function adjustedCoords(coords) {
+//     let newCoords = [];
+//     newCoords.push(coords[0])
+//     newCoords.push(coords[1] + 0.001)
+//     return newCoords;
+// }
+// const adjustedRinkCoords = adjustedCoords(rinkCoords)
 
-function getDirections() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition)
-  }
-}
-
-function showPosition(position) {
-  let coords = [];
-  coords.push(position.coords.longitude);
-  coords.push(position.coords.latitude);
-  directions.setOrigin(coords);
-  directions.setDestination(rinkCoords)
-}
-
-// Shift the map south slightly to move marker out from behind directions
-function adjustedCoords(coords) {
-    let newCoords = [];
-    newCoords.push(coords[0])
-    newCoords.push(coords[1] + 0.001)
-    return newCoords;
-}
-const adjustedRinkCoords = adjustedCoords(rinkCoords)
-
-
+// Mapbox map creation
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: adjustedRinkCoords,
+    center: rinkCoords,
     zoom: 13.75
 });
 
-let directions = new MapboxDirections({
-    accessToken: mapToken,
-    unit: 'metric',
-    profile: 'mapbox/driving',
-    controls: {
-        profileSwitcher: false,
-        instructions: false
-    }
-});
+// // Mapbox-directions configuration
+// let directions = new MapboxDirections({
+//     accessToken: mapToken,
+//     unit: 'metric',
+//     profile: 'mapbox/driving',
+//     controls: {
+//         profileSwitcher: false,
+//         instructions: false
+//     }
+// });
 
+// Map controls and popup configuration
 map.addControl(new mapboxgl.NavigationControl());
-map.addControl(directions, 'top-left');
+// map.addControl(directions, 'top-left');
 new mapboxgl.Marker()
-    .setLngLat(rink.geometry.coordinates)
+    .setLngLat(rinkCoords)
     .setPopup(
         new mapboxgl.Popup({offset: 25})
         .setHTML(
